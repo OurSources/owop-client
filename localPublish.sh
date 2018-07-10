02b7d4d8623e4ea35bf7ed44d2747625b9466d6c
OurSources <OurSources@users.noreply.github.com>
2018-07-10 11:01:33 -0400

    local publish fixed

--- localPublish.sh
+++ localPublish.sh
@@ -4,4 +4,4 @@ echo "Going to build and PUBLISH the OWOP client, press enter to confirm!"
 read
 set -x
 npm run release
-rsync -vr --delete --max-delete=1 ./dist/ /var/www/owop/
+su -c "rsync -vr --delete --max-delete=1 $(pwd)/dist/ /var/www/owop/" -
