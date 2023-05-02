6c2829236ac5a81bd8729951663e67a687c6574f
Cygnus <88682110+Ceuthyn@users.noreply.github.com>
2023-05-02 23:22:50 +0300

    Update canvas_renderer.js

--- src/js/canvas_renderer.js
+++ src/js/canvas_renderer.js
@@ -551,10 +551,14 @@ function getCenterPixel() {
 }
 
 function centerCameraTo(x, y) {
-	if(typeOf(x) != "number" || isNaN(x)){x = 0;}
-	if(typeOf(y) != "number" || isNaN(y)){y = 0;}
-	cameraValues.x = -(window.innerWidth / camera.zoom / 2) + x;
-	cameraValues.y = -(window.innerHeight / camera.zoom / 2) + y;
+	if(typeOf(x) == "number" || !isNaN(x)){
+		cameraValues.x = -(window.innerWidth / camera.zoom / 2) + x;
+	}
+	
+	if(typeOf(y) == "number" || !isNaN(y)){
+		cameraValues.y = -(window.innerHeight / camera.zoom / 2) + y;
+	}
+	
 	onCameraMove();
 }
 
